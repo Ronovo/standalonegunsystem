159c280c3c8c9ed9099f425ad18c92388d52c9c5
Ronovo <ian.kent91@gmail.com>
2025-02-17 10:55:28 -0600

    Added Total Stats to Bulk Fire Report, In preperation to build balance testing report

--- Screens/bulk.py
+++ Screens/bulk.py
@@ -21,11 +21,30 @@ def bulkShots():
     # Ask For Distance
     newDummy = dummy_preset.getNewDummy(False)
 
-    reportArray = getReportArray(selectedWeapon, magazineCount, newDummy)
+    #Result = [MagazineArray, TotalArray]
+    #Each Array has Hits,Misses,Dummies Destroyed, and Damage, in order
+    reportResult = getReport(selectedWeapon, magazineCount, newDummy)
 
-    for x in reportArray:
+    #Print Report Per Magazine
+    magazineArray = reportResult[0]
+    print("Breakdown by Magazine")
+    print("--------------------")
+    for x in magazineArray:
         print(x)
-    print("\n")
+    print("")
+    #Get Total Variables
+    totalArray = reportResult[1]
+    totalHits = totalArray[0]
+    totalMiss = totalArray[1]
+    totalShots = totalHits + totalMiss
+    accuracy = totalHits / totalShots
+    totalDummy = totalArray[2]
+    totalDamage = totalArray[3]
+    print("Totals for All Shots")
+    print("--------------------")
+    print(str(totalHits) + " Hits / " + str(totalMiss) + " Misses / "
+        + str(totalDummy) + " Dummies Destroyed / " + str(totalDamage) + " Damage Done")
+    print("")
 
 
 def getTotalMagazineCount(selectedWeapon):
@@ -39,10 +58,10 @@ def getTotalMagazineCount(selectedWeapon):
     else:
         magazineCount = int(answer) // maxAmmo
         extraBullets = int(answer) % maxAmmo
-    print("You have selected " + str(magazineCount) + " full magazines")
+    print("\nYou have selected " + str(magazineCount) + " full magazines")
     if extraBullets > 0:
         magazineCount += 1
-        print("You also have a non-full magazine with " + str(extraBullets) + " in it.")
+        print("You also have a non-full magazine with " + str(extraBullets) + " in it.\n")
 
     # Ammo Count
     if extraBullets > 0:
@@ -53,10 +72,17 @@ def getTotalMagazineCount(selectedWeapon):
     results = [magazineCount,selectedWeapon]
     return results
 
-def getReportArray(selectedWeapon, magazineCount, newDummy):
+def getReport(selectedWeapon, magazineCount, newDummy):
+    # Variables needed to build final report
     n = 1
-    reportArray = []
+    magazineArray = []
+    totalHits = 0
+    totalMisses = 0
+    totalDamage = 0
+    totalDummy = 0
 
+    #Loop through Bullets Selected (Ammo in Gun + magazine Count)
+    #Magazine Outer Loop - Total magazines
     while magazineCount > 0:
         magazineCount -= 1
         hits = 0
@@ -64,6 +90,7 @@ def getReportArray(selectedWeapon, magazineCount, newDummy):
         deadDummy = 0
         magDamage = 0
         magazineString = "Magazine " + str(n) + ": "
+        #Magazine Inner Loop - Bullets in Gun
         while selectedWeapon.currentAmmo > 0:
             selectedWeapon.fireShot(False)
             result = hitCalculations.simpleHit(selectedWeapon, newDummy, False)
@@ -71,16 +98,28 @@ def getReportArray(selectedWeapon, magazineCount, newDummy):
                 hits += 1
                 magDamage += selectedWeapon.damage
                 newDummy.takeDamage(selectedWeapon.damage)
+                totalDamage += selectedWeapon.damage
                 if newDummy.health <= 0:
                     deadDummy += 1
                     newDummy.health = 200
             else:
                 misses += 1
-
+        #Update Loop Variables after Weapon is Empty
+        totalHits += hits
+        totalMisses += misses
+        totalDummy += deadDummy
         if magazineCount > 0:
             selectedWeapon.reload()
-        magazineString = (magazineString + str(hits) + " hits/ " + str(misses) + " misses/ "
-                          + str(deadDummy) + " Dummies Destroyed/ " + str(magDamage) + " Damage Done")
-        reportArray.append(magazineString)
+        magazineString = (magazineString + str(hits) + " Hits / " + str(misses) + " Misses / "
+                          + str(deadDummy) + " Dummies Destroyed / " + str(magDamage) + " Damage Done")
+        magazineArray.append(magazineString)
         n += 1
-    return reportArray
+
+    #Build Total Array
+    totalArray = [totalHits,totalMisses,totalDummy,totalDamage]
+
+    #Build Final Report
+    # 0. Magazine Array - List of Magazine Stats
+    # 1. Total Array - List of Total Stats for report
+    result = [magazineArray,totalArray]
+    return result
