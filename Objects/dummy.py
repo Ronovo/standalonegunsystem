517073d6af00b2a44e115d4bef0d55b045c50c4f
Ronovo <ian.kent91@gmail.com>
2025-02-16 20:31:17 -0600

    V1.1 Release Bulk Fire Testing Report

---
+++ Objects/dummy.py
@@ -0,0 +1,44 @@
+class Dummy:
+    def __init__(self, health, distance):
+        self.health = health
+        self.distance = distance
+
+    def takeDamage(self, damage):
+        self.health -= damage
+
+    def setDummyRange(self, debug):
+        dFlag = False
+        dummyRange = 0
+        while not dFlag:
+            print("Please select distance for dummy")
+            print("----------")
+            print("1.) 50 Meters")
+            print("2.) 100 Meters")
+            print("3.) 150 Meters")
+            print("4.) 200 Meters")
+            print("5.) 300 Meters")
+            print("6.) 400 Meters")
+            print("7.) 500 Meters")
+            print("8.) return \n")
+            answer = input("Pick 1-8\n")
+            numAnswer = int(answer)
+            if 0 < numAnswer < 5:
+                dummyRange = 50 * numAnswer
+                dFlag = True
+            elif 4 < numAnswer < 8:
+                dFlag = True
+                match numAnswer:
+                    case 5:
+                        dummyRange = 300
+                    case 6:
+                        dummyRange = 400
+                    case 7:
+                        dummyRange = 500
+            elif numAnswer == 8:
+                quit()
+            else:
+                print("Incorrect choice. Try again.")
+        self.distance = dummyRange
+        if debug:
+            print("Dummy has been set to range of " + str(dummyRange))
+
---
+++ Screens/bulk.py
@@ -0,0 +1,88 @@
+import hitCalculations
+from CommonScripts import weaponType
+from Objects import weapons
+from Objects.dummy import Dummy
+
+weaponList = []
+mode = 'Single Shot'
+destroyedDummy = 0
+
+def bulkShots():
+    print("Bulk Report")
+
+    # Ask for gun
+    selectedWeapon = weaponType.getSelectedWeapon()
+    if selectedWeapon is None:
+        return
+
+    results = getTotalMagazineCount(selectedWeapon)
+    magazineCount = results[0]
+    selectedWeapon = results[1]
+
+
+    # Ask For Distance
+    newDummy = Dummy(200,0)
+    newDummy.setDummyRange(False)
+
+    reportArray = getReportArray(selectedWeapon, magazineCount, newDummy)
+
+    for x in reportArray:
+        print(x)
+
+
+def getTotalMagazineCount(selectedWeapon):
+    maxAmmo = selectedWeapon.maxAmmo
+
+    print("Your weapon currently has " + str(maxAmmo) + " bullets per magazine.")
+    # Ask for number of shots/magazines
+    answer = input("How many shots do you want to test?\n")
+    if int(answer) < maxAmmo:
+        extraBullets = int(answer)
+    else:
+        magazineCount = int(answer) // maxAmmo
+        extraBullets = int(answer) % maxAmmo
+    print("You have selected " + str(magazineCount) + " full magazines")
+    if extraBullets > 0:
+        magazineCount += 1
+        print("You also have a non-full magazine with " + str(extraBullets) + " in it.")
+
+    # Ammo Count
+    if extraBullets > 0:
+        selectedWeapon.currentAmmo = int(extraBullets)
+    else:
+        selectedWeapon.currentAmmo = selectedWeapon.maxAmmo
+
+    results = [magazineCount,selectedWeapon]
+    return results
+
+def getReportArray(selectedWeapon, magazineCount, newDummy):
+    n = 1
+    reportArray = []
+
+    while magazineCount > 0:
+        magazineCount -= 1
+        hits = 0
+        misses = 0
+        deadDummy = 0
+        magDamage = 0
+        magazineString = "Magazine " + str(n) + ": "
+        while selectedWeapon.currentAmmo > 0:
+            selectedWeapon.fireShot(False)
+            result = hitCalculations.simpleHit(selectedWeapon, newDummy.distance, False)
+            if result == 1:
+                hits += 1
+                magDamage += selectedWeapon.damage
+                newDummy.takeDamage(selectedWeapon.damage)
+                if newDummy.health <= 0:
+                    deadDummy += 1
+                    newDummy.health = 200
+            else:
+                misses += 1
+
+        if magazineCount > 0:
+            selectedWeapon.reload()
+        magazineString = (magazineString + str(hits) + " hits/ " + str(misses) + " misses/ "
+                          + str(deadDummy) + " Dummies Destroyed/ " + str(magDamage) + " Damage Done")
+        reportArray.append(magazineString)
+        n += 1
+    return reportArray
